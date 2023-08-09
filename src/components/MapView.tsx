ed546e4f7ca5283c3a05def59f826891ca693fab
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-09 00:34:10 -0500

    Add view mapa

---
+++ src/components/MapView.tsx
@@ -0,0 +1,41 @@
+import { useContext, useLayoutEffect, useRef } from "react";
+import { PlacesContext } from "../context";
+import { Loading } from ".";
+import mapboxgl from "mapbox-gl";
+
+export const MapView = () => {
+  const { isLoading, userLocation } = useContext(PlacesContext);
+
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new mapboxgl.Map({
+        container: mapDiv.current!,
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: userLocation,
+        zoom: 12,
+      });
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div
+      ref={mapDiv}
+      style={{
+        backgroundColor: "red",
+        height: "100vh",
+        width: "100vw",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    >
+      {userLocation?.join(",")}
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmljaGFyZC1yZXllcyIsImEiOiJjbGwzOXE4emcxa2IwM3Jsc2MyZTR0NTV1In0.uAQCbH6jltO1CzTZ6AJ_Og";
+
+if (!navigator.geolocation) {
+  alert("Tu navegador no tiene opción de Geolocation");
+  throw new Error("Tu navegador no tiene opción de Geolocation");
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<MapsApp />);
